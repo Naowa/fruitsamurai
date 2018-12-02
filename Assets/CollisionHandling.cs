839f1c1ca6239a406abac0ba676c608596e2b2c3
Ian Cooperman <i.perry.leonard.c@gmail.com>
2018-12-02 14:20:46 -0800

    Adding rudimentary slicing and smushing methods to the fruit prefabs.

---
+++ Assets/CollisionHandling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandling : MonoBehaviour {
+
+	public Transform fruitTransform;
+	public 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Smush () {
+		fruitTransform.localScale -= new Vector3(0, 0.8f, 0);
+	}
+
+	void Slice () {
+		Destroy(gameObject);
+	}
+}
